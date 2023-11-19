47c0d206f3bb1525baac33abc0125f58afc54615
DPOH-VAR <DPOH-VAR@users.noreply.github.com>
2023-11-19 04:19:19 +0000

    deploy: 439edfbd7dde651bfdffdcafe8513dd21423abba

--- game.008a4f21.js
+++
@@ -1,3 +0,0 @@
-(0,globalThis.parcelRequire1811.register)("a6Mfq",function(e,n){Object.defineProperty(e.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(e.exports,"default",{get:()=>o,set:void 0,enumerable:!0,configurable:!0});var o='import room from "varhub:room";\nroom.addEventListener("join", function (a) {\n  console.log("JOIN", a.client, a.message), a.client.name = a.message.name;\n});\nexport function send(a) {\n  return room.broadcast("message", this.client.name, a), !0;\n}'});//# sourceMappingURL=game.008a4f21.js.map
-
-//# sourceMappingURL=game.008a4f21.js.map
