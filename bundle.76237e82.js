5fef5ae49ef84df2280f8e2262c3dba2378a6477
DPOH-VAR <DPOH-VAR@users.noreply.github.com>
2023-11-23 00:54:45 +0000

    deploy: 5ee30ff057bf3c537af571ef05a2c34059ac1bd8

--- bundle.76237e82.js
+++
@@ -1,3 +0,0 @@
-(0,globalThis.parcelRequire1811.register)("ke64w",function(e,o){Object.defineProperty(e.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(e.exports,"default",{get:()=>r,set:void 0,enumerable:!0,configurable:!0});var r='import data from "./data.json";\nconsole.log("b");\nexport default data;\n'});//# sourceMappingURL=bundle.76237e82.js.map
-
-//# sourceMappingURL=bundle.76237e82.js.map
